126b6accd52bf0d68e808e7b5e35e640ee20f2ff
Maru Skyborne <mariesfine@gmail.com>
2025-02-11 06:00:14 -0600

    Move onSample to the bottom

--- www/app/js/motd-js.php
+++ www/app/js/motd-js.php
@@ -73,24 +73,23 @@ var motd = {
         $("#motd-output").show();
     },
 
-    onSampleData: function () {
-        $("#motdInput").val(<?=$sampleData?>);
-        // $("#motdInput").val("[03:17:57] EVE System > Channel MOTD: <font size=\"14\" color=\"#bfffffff\"></font><font size=\"12\" color=\"#ffffffff\">.<br><br></font><font size=\"14\" color=\"#ffffffff\">This channel is not used. Please use the below channels:<br><br></font><font size=\"14\" color=\"#ff6868e1\"><a href=\"joinChannel:player_1262619e7bd511eb93929abe94f5a167\">Loose Coalition</a></font><font size=\"14\" color=\"#ffffffff\"> | for combined Coalition comms<br>Password: </font><font size=\"14\" color=\"#ffffff00\">arborealManapua_9599<br><br></font><font size=\"14\" color=\"#ff6868e1\"><a href=\"joinChannel:player_495464cfc22911efa27d00109bd0fca8\">Loose Ops</a></font><font size=\"14\" color=\"#ffffffff\"> | for combined ops with blues, allies, & friends of convenience<br>Password: </font><font size=\"14\" color=\"#ffffff00\">efervescentAnaconda_588<br><br></font><font size=\"14\" color=\"#ff6868e1\"><a href=\"joinChannel:player_297c0e4f6a8611eb95769abe94f5a43f\">Fast 'n' Loose</a></font><font size=\"14\" color=\"#ffffffff\"> | pub chat<br><br></font><font size=\"12\" color=\"#ffffffff\">ver. 2025.01.04<br><br>.</font>");
-    },
-
     onDismiss: function () {
         // empty input
         $("#motdInput").val("");
-
+        
         // toggle view
         $("#motd-ui").show();
         $("#motd-output").hide();
     },
-
+    
     onCopy: function () {
         var password = $(this).closest("tr").find(".password").text();
         console.log("password is", password);
         site.copyToClipboard(password);
+    },
+    
+    onSampleData: function () {
+        $("#motdInput").val(<?=$sampleData?>);
     }
 };
 
